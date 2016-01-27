1bb5af3a52b2ee9867c5531bf96ab901e25afe36
lyt217 <lyt217@naver.com>
2016-01-28 01:18:08 +0900

    testing ..

--- app.js
+++ app.js
@@ -16,15 +16,33 @@
 // }
 // helloWorld();
 var express = require('express');
-
+var path = require('path');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", 'ejs');
+app.use(express.static(path.join(__dirname,'/public')));
+
+var data={count:0};
 
 app.get('/', function (req,res){
-  res.send('Hello World');
+  data.count++;
+  // res.send('Hello World');
+  res.render('main_ejs',data);
 });
-
+app.get('/reset', function (req,res){
+  data.count = 0;
+  res.render('main_ejs',data);
+});
+app.get('/set/count',function (req,res){
+  if(req.query.key){
+      data.count = req.query.key;
+  }
+  res.render('main_ejs',data);
+});
+app.get('/set/:num',function (req,res){
+  data.count = req.params.num;
+  res.render('main_ejs',data);
+});
 app.listen(3000, function(){
   console.log('Server Started!');
 });
